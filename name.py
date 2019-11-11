b1074eb0ed56a226476cebc3ebee3cdcbbdc3a71
Tarik Tabakovic <tariktabakovic@Tariks-MacBook-Air.local>
2019-11-11 14:23:12 -0500

    totally amazing solves that one problem

---
+++ name.py
@@ -0,0 +1,15 @@
+
+name = input("what is your name?")
+print ("Hello," + name.upper() + "!")
+
+text = "Number of charcters in this text"
+print (len(text))
+
+
+subject = input("what is your favorite subject?")
+
+print ( name + " favorite subject in school is " + subject)
+
+print ("What is name? " + name )
+print ("What is subject? " + subject)
+print (name + " favorite subject in school is " + subject)
